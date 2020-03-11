bd7c92d9f21b22b99094778f67985329638fbd58
Tetyushev <tetav@mail.ru>
2020-03-11 22:46:05 +0300

    test

---
+++ src/test/java/ru/myhelphelp/test/Main.java
@@ -0,0 +1,13 @@
+package ru.myhelphelp.test;
+
+import org.junit.Test;
+
+public  class Main {
+    public static void Main (String [] args){
+        System.out.println("Hello Word!");
+    }
+    @Test
+    public void test1(){
+        System.out.printf("Test 1");
+    }
+}
